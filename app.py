a422521df03c91e1706fc1a9e7b8239fd8bde0d6
DanielLaborda <dani.laborda90@gmail.com>
2021-06-29 22:16:05 +0200

    request company to heroku api

--- app.py
+++ app.py
@@ -122,7 +122,7 @@ def home():
     return f'Its working'
 
 #company
-@app.route('/company', methods=['GET'])
+@app.route('/company/', methods=['GET'])
 def get_company():
     _id = request.args['id']
     company = Company.query.get(_id)
