1af5317cca45b184a71717916aca37657775de00
DanielLaborda <dani.laborda90@gmail.com>
2021-06-30 23:14:02 +0200

    testing with heroku api

--- app.py
+++ app.py
@@ -13,41 +13,41 @@ db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
 # MODEL COMPANY
-class Company(db.Model):
-    company_id = db.Column(db.Integer, primary_key=True)
-    company_name = db.Column(db.String(45))
-    company_logo = db.Column(db.BLOB)
-    company_description = db.Column(db.Text)
-    company_contact = db.Column(db.String(11))
+# class Company(db.Model):
+#     company_id = db.Column(db.Integer, primary_key=True)
+#     company_name = db.Column(db.String(45))
+#     company_logo = db.Column(db.BLOB)
+#     company_description = db.Column(db.Text)
+#     company_contact = db.Column(db.String(11))
     
-    def __init__(self,  company_name, company_logo, company_description, company_contact):
-        self.company_name = company_name
-        self.company_logo = company_logo
-        self.company_description = company_description
-        self.company_contact = company_contact
+#     def __init__(self,  company_name, company_logo, company_description, company_contact):
+#         self.company_name = company_name
+#         self.company_logo = company_logo
+#         self.company_description = company_description
+#         self.company_contact = company_contact
 
 # # MODEL USERTYPE
-# class UsersType(db.Model):
-#     userstype_id = db.Column(db.Integer, primary_key=True)
-#     userstype_name = db.Column(db.String(45))
-#     def __init__(self, userstype_name):
-#         self.userstype_name = userstype_name
+class UsersType(db.Model):
+    userstype_id = db.Column(db.Integer, primary_key=True)
+    userstype_name = db.Column(db.String(45))
+    def __init__(self, userstype_name):
+        self.userstype_name = userstype_name
 
 # # MODEL USERS
-# class Users(db.Model):
-#     users_id = db.Column(db.Integer, primary_key=True)
-#     users_name = db.Column(db.String(50))
-#     users_surname = db.Column(db.String(50))
-#     users_password = db.Column(db.String(25))
-#     users_email = db.Column(db.String(50))
-#     users_type = db.Column(db.Integer)
+class Users(db.Model):
+    users_id = db.Column(db.Integer, primary_key=True)
+    users_name = db.Column(db.String(50))
+    users_surname = db.Column(db.String(50))
+    users_password = db.Column(db.String(25))
+    users_email = db.Column(db.String(50))
+    users_type = db.Column(db.Integer)
 
-#     def __init__(self, users_name, users_surname, users_password, users_email, users_type):
-#         self.users_name = users_name
-#         self.users_surname = users_surname
-#         self.users_password = users_password
-#         self.users_email = users_email
-#         self.users_type = users_type
+    def __init__(self, users_name, users_surname, users_password, users_email, users_type):
+        self.users_name = users_name
+        self.users_surname = users_surname
+        self.users_password = users_password
+        self.users_email = users_email
+        self.users_type = users_type
 
 # # MODEL GARAGE
 # class Garage(db.Model):
@@ -108,11 +108,11 @@ class Company(db.Model):
 #         self.categoriesracing_video = categoriesracing_video
 
 # #SCHEMA
-# class UserTypesSchema(ma.Schema):
-#     class Meta:
-#         fields = ('userstype_id', 'userstype_name')
-# userType_schema = UserTypesSchema()
-# userTypes_schema = UserTypesSchema(many=True)
+class UserTypesSchema(ma.Schema):
+    class Meta:
+        fields = ('userstype_id', 'userstype_name')
+userType_schema = UserTypesSchema()
+userTypes_schema = UserTypesSchema(many=True)
 
 
 #ROUTES
@@ -122,28 +122,28 @@ def home():
     return f'Its working'
 
 #company
-@app.route('/company/', methods=['GET'])
-def get_company():
-    _id = request.args['id']
+# @app.route('/company/', methods=['GET'])
+# def get_company():
+#     _id = request.args['id']
 
-    company = Company.query.get(_id)
+#     company = Company.query.get(_id)
   
-    result = {
-        "company_name": company.company_name,
-        "company_description": company.company_description,
-        "company_contact": company.company_contact,
-        "company_logo": base64.b64encode(company.company_logo).decode("utf-8") 
-    }
-    db.session.commit()
-    response = jsonify(result)
-    return response
+#     result = {
+#         "company_name": company.company_name,
+#         "company_description": company.company_description,
+#         "company_contact": company.company_contact,
+#         "company_logo": base64.b64encode(company.company_logo).decode("utf-8") 
+#     }
+#     db.session.commit()
+#     response = jsonify(result)
+#     return response
     
 
 # #user_types
-# @app.route('/user_types/<_id>', methods=['GET'])
-# def get_userTypes_id(_id):
-#     userTypes = UsersType.query.get(_id)
-#     return userType_schema.jsonify(userTypes)
+@app.route('/user_types/<_id>', methods=['GET'])
+def get_userTypes_id(_id):
+    userTypes = UsersType.query.get(_id)
+    return userType_schema.jsonify(userTypes)
 
 # # USER- INFO by id
 # @app.route('/userInfo/<_id>', methods=['GET'])
