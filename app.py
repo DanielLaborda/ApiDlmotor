3cd649ec3eb2a4834ba65794d485fc78fa56adb2
DanielLaborda <dani.laborda90@gmail.com>
2021-07-01 19:18:24 +0200

    commit to return version

--- app.py
+++ app.py
@@ -37,14 +37,14 @@ def home():
 @app.route('/company/', methods=['GET'])
 def get_company():
     _id = request.args['id']
-    company = Company.query.get(_id)
+    # company = Company.query.get(_id)
   
-    result = {
-        "company_name": company.company_name,
-        "company_description": company.company_description,
-        "company_contact": company.company_contact,
-        "company_logo":  base64.b64encode(company.company_logo).decode("utf-8")   
-    }
-    db.session.commit()
-    response = jsonify(result)
-    return response
+    # result = {
+    #     "company_name": company.company_name,
+    #     "company_description": company.company_description,
+    #     "company_contact": company.company_contact,
+    #     "company_logo":  base64.b64encode(company.company_logo).decode("utf-8")   
+    # }
+    # db.session.commit()
+    # response = jsonify(result)
+    return f'response' + _id
