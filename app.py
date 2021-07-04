f0c51b518a2df2938cc534957ffbcf4cfbea1500
DanielLaborda <dani.laborda90@gmail.com>
2021-07-04 17:53:16 +0200

    garage and racing team to heroku

--- app.py
+++ app.py
@@ -49,6 +49,64 @@ class Users(db.Model):
         self.users_email = users_email
         self.users_type = users_type
 
+# MODEL GARAGE
+class Garage(db.Model):
+    garage_id = db.Column(db.Integer, primary_key=True)
+    garage_name = db.Column(db.String(45))
+    garage_description = db.Column(db.Text)
+    garage_contact = db.Column(db.String(11))
+    garage_positionX = db.Column(db.FLOAT)
+    garage_positionY = db.Column(db.FLOAT)
+    garage_image = db.Column(db.BLOB)
+  
+    def __init__(self,  garage_name, garage_description, garage_contact, garage_positionX, garage_positionY, garage_image):
+        self.garage_name = garage_name
+        self.garage_description = garage_description
+        self.garage_contact = garage_contact
+        self.garage_positionX = garage_positionX
+        self.garage_positionY = garage_positionY
+        self.garage_image = garage_image
+
+# MODEL RacingTeam
+class Racingteam(db.Model):
+    racingteam_id = db.Column(db.Integer, primary_key=True)
+    racingteam_name = db.Column(db.String(50))
+    racingteam_slogan = db.Column(db.String(100))
+    racingteam_description = db.Column(db.Text)
+    racingteam_description2 = db.Column(db.Text)
+    racingteam_video = db.Column(db.String(100))
+    
+    def __init__(self,  racingteam_name, racingteam_slogan, racingteam_description, racingteam_description2, racingteam_video):
+        self.racingteam_name = racingteam_name
+        self.racingteam_slogan = racingteam_slogan
+        self.racingteam_description = racingteam_description
+        self.racingteam_description2 = racingteam_description2
+        self.racingteam_video = racingteam_video
+
+# MODEL ImagesBannerRacing
+class Imagesbannerracing(db.Model):
+    ImagesBannerRacing_id = db.Column(db.Integer, primary_key=True)
+    ImagesBannerRacing_image = db.Column(db.BLOB)
+    ImagesBannerRacing_racingteam = db.Column(db.Integer)
+
+    def __init__(self,  ImagesBannerRacing_id, ImagesBannerRacing_image, ImagesBannerRacing_racingteam):
+        self.ImagesBannerRacing_id = ImagesBannerRacing_id
+        self.ImagesBannerRacing_image = ImagesBannerRacing_image
+        self.ImagesBannerRacing_racingteam = ImagesBannerRacing_racingteam
+
+# MODEL Categoriesracing
+class Categoriesracing(db.Model):
+    categoriesracing_id = db.Column(db.Integer, primary_key=True)
+    categoriesracing_name = db.Column(db.String(50))
+    categoriesracing_image = db.Column(db.BLOB)
+    categoriesracing_video = db.Column(db.String(100))
+
+    def __init__(self,  categoriesracing_id, categoriesracing_name, categoriesracing_image, categoriesracing_video):
+        self.categoriesracing_id = categoriesracing_id
+        self.categoriesracing_name = categoriesracing_name
+        self.categoriesracing_image = categoriesracing_image
+        self.categoriesracing_video = categoriesracing_video
+
 #SCHEMA
 class UserTypesSchema(ma.Schema):
     class Meta:
@@ -208,3 +266,46 @@ def create_user():
     response = jsonify(result)
     response.headers["Access-Control-Allow-Origin"] = "*"
     return  response
+
+#garage
+@app.route('/garage/', methods=['GET'])
+def get_garage():
+    _id = request.args['id']  
+    garage = Garage.query.get(_id)
+    result = {
+        "garage_name": garage.garage_name,
+        "garage_description": garage.garage_description,
+        "garage_contact": garage.garage_contact,
+        "garage_position": [garage.garage_positionX, garage.garage_positionY],
+        "garage_image":  base64.b64encode(garage.garage_image).decode("utf-8")   
+    }
+  
+    db.session.commit()
+    response = jsonify(result)
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    return response
+
+#racingTeam
+@app.route('/racingTeam/', methods=['GET'])
+def get_racingTeam():
+    _id = request.args['id']  
+    racingTeam = Racingteam.query.get(_id)
+    imagesBanner = Imagesbannerracing.query.filter(Imagesbannerracing.ImagesBannerRacing_racingteam == _id).all()
+    
+    resultImages = []
+    for image in imagesBanner:
+        resultImages.append(base64.b64encode(image.ImagesBannerRacing_image).decode("utf-8") )
+    
+    result = {
+        "racingTeam_name": racingTeam.racingteam_name,
+        "racingTeam_slogan": racingTeam.racingteam_slogan,
+        "racingTeam_description": racingTeam.racingteam_description,
+        "racingTeam_description2": racingTeam.racingteam_description2,
+        "racingTeam_video": racingTeam.racingteam_video,
+        "racingTeam_imagesBanner":  resultImages  
+    }
+    
+    db.session.commit()
+    response = jsonify(result)
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    return response   
