635c8292119dbbbba3b5628996a3e1be54b28807
DanielLaborda <dani.laborda90@gmail.com>
2021-07-05 04:08:48 +0200

    testing vehicles to heroku

--- app.py
+++ app.py
@@ -473,14 +473,14 @@ def get_vehicles():
         
         for rim in rims:
             print(rim.rimsvehicles_material)
-            rimsVehicles.append({
-                'rims_id': rim.rimsvehicles_id,
-                'rims_model': rim.rimsvehicles_model,
-                'rims_size': rim.rimsvehicles_size,
-                'rims_material': rim.rimsvehicles_material
-            #     'rims_image': base64.b64encode(rim.rimsvehicles_image).decode("utf-8"),
-            #     'rims_baseprice': rim.rimsvehicles_baseprice
-            })
+            # rimsVehicles.append({
+            #     'rims_id': rim.rimsvehicles_id,
+            #     'rims_model': rim.rimsvehicles_model,
+            #     'rims_size': rim.rimsvehicles_size,
+            #     'rims_material': rim.rimsvehicles_material
+            # #     'rims_image': base64.b64encode(rim.rimsvehicles_image).decode("utf-8"),
+            # #     'rims_baseprice': rim.rimsvehicles_baseprice
+            # })
 
         result.append({
             "vehicles_id": vehicle.vehicles_id,
