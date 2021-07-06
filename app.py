161e47b0d729ece69790aa2d9a65a44d9a379aee
DanielLaborda <dani.laborda90@gmail.com>
2021-07-06 17:59:10 +0200

    testing cors to heroku

--- app.py
+++ app.py
@@ -393,6 +393,7 @@ def get_userinfo():
 
 # USER- create
 @app.route('/createUsers/', methods=['POST'])
+@cross_origin()
 def create_user():
     users_name = request.json['users_name']
     users_surname = request.json['users_surname']
